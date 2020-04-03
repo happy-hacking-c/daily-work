e4e96097946a4c2e45157d58d5f4e30dfb4e0aba
wuym1 <wuym1@ziroom.com>
2020-04-03 17:13:34 +0800

    first

--- 2020-04-07/wuym1/wuym1/main.c
+++ 2020-04-07/wuym1/wuym1/main.c
@@ -1,6 +1,6 @@
 //
 //  main.c
-//  HelloWorld
+//  wuym1
 //
 //  Created by min on 2020/4/3.
 //  Copyright © 2020 min. All rights reserved.
@@ -11,10 +11,5 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
-    
-    // hard版本
-    char str[20] = "Hello, World!";
-    printf("%s\n", str);
-    
     return 0;
 }
