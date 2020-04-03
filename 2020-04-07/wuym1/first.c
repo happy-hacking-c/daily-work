f80cd5b54e8ab7fd991949d08bb3a9ad2143538e
wuym1 <wuym1@ziroom.com>
2020-04-03 16:57:54 +0800

    first

---
+++ 2020-04-07/wuym1/first.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by min on 2020/4/3.
+//  Copyright © 2020 min. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    
+    // hard版本
+    char str[20] = "Hello, World!";
+    printf("%s\n", str);
+    
+    return 0;
+}
