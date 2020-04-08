d37f4418221a94c051add297f2f9c69d6854e965
wuym1 <wuym1@ziroom.com>
2020-04-08 12:47:10 +0800

    up

---
+++ 2020-04-08/wuym1/wuym1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  wuym1
+//
+//  Created by min on 2020/4/8.
+//  Copyright © 2020 min. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
