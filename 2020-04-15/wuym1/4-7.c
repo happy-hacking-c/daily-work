0024babdb41be56188485a393b35e9d655702f00
wuym1 <wuym1@ziroom.com>
2020-04-15 15:35:18 +0800

    cp

--- 2020-04-15/wuym1/4-7.c
+++ 2020-04-15/wuym1/4-7.c
@@ -1,8 +1,32 @@
-//
+// create by wuym1 hahaha
 // 4-7
 
 #include "stdio.h"
 
+#define BUFSIZE 100
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+void ungetch(int c) {
+    if (bufp >= BUFSIZE) {
+        printf("ungetch: too many characters\n");
+    } else {
+        buf[bufp++] = c;
+    }
+}
+
+void ungets(char s[]) {
+    int i = 0;
+    while (s[i] != '\0') {
+        ungetch(s[i]);
+        i++;
+    }
+}
+
 int main() {
+    char s[100] = "hello world";
+    ungets(s);
+    printf("%s\n", buf);
     return 0;
 }
