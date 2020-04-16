a681ea7bd574e5191d20cbd576861b082985fc71
zhenwx <allowall@163.com>
2020-04-16 14:41:16 +0800

    .

---
+++ 2020-04-16/zhenwx/Q4-14.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+#define swap(t, x, y) t temp; temp = x; x = y; y = temp;
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+
+    printf("swapBefore: a = %d, b = %d\n", a, b);
+    swap(int, a, b);
+    printf("swapAfter: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
+
+
