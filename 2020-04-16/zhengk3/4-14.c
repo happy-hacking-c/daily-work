827ff3b51751d6f490d71e49f28a6f8946fa5cc9
zkbhj <zhengk3@ziroom.com>
2020-04-16 19:11:37 +0800

    初始化0417作业目录

--- 2020-04-16/zhengk3/4-14.c
+++ 2020-04-16/zhengk3/4-14.c
@@ -1,37 +1,14 @@
 
 /**
- * 第8堂课作业：4-14
+ * 第9堂课作业：5-2
  * 郑凯
- * 2020年4月16日
+ * 2020年4月17日
  * */
 
 #include <stdio.h>
-#include <string.h>
-
-#define swap(t, x, y) t tp; tp = x; x = y; y = tp;
-#define myPrint(x) printf(#x " = %d\n", x);
 
 int main()
 {
-
-    // int a = 100;
-    // int b = 50;
-
-    // swap(int, a, b);
-
-    // myPrint(a);
-    // myPrint(b);
-
-
-    char c = 'a';
-    char d = 'b';
-
-    swap(char, c, d);
-
-    myPrint(c);
-    myPrint(d);
-
-
     return 0;
 }
 
