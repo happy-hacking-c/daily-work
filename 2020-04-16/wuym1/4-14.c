5f6fad5d508b35302199e54579ed939d995e313f
wuym1 <wuym1@ziroom.com>
2020-04-16 17:39:21 +0800

    cp

---
+++ 2020-04-16/wuym1/4-14.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define swap(t, x, y) t temp; temp = x; x = y; y = temp;
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+
+    printf("swapBefore: a = %d, b = %d\n", a, b);
+    swap(char, a, b);
+    printf("swapAfter: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
