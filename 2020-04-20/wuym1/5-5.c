1b2dd3cb888ec79c795e24e3e10243262ac5de35
wuym1 <wuym1@ziroom.com>
2020-04-20 17:59:23 +0800

    cp

--- 2020-04-20/wuym1/5-5.c
+++ 2020-04-20/wuym1/5-5.c
@@ -1,6 +1,34 @@
 #include "stdio.h"
 
+void strncpy(char *s, char *t, int n) {
+    int i = 0;
+    while (i < n && (*s++ = *t++)) {
+        i++;
+    }
+}
+
+void strncat(char *s, char *t, int n) {
+    char *temp = s + n;
+    while (*temp++ = *t++) {
+        /* code */
+    }
+}
+
+char strncmp(char *s, char *t, int n) {
+    for(; *s == *t; ++s, ++t){
+        if(*s == '\0' || --n <= 0)
+            return 0;
+    }
+    return *s - *t;
+}
+
 int main() {
-    printf("11112222");
+    char *s = "11111";
+    char *t = "12345";
+    int n = 3;
+
+    strncat(s, t, n);
+    printf("%s", s);
+
     return 0;
 }
