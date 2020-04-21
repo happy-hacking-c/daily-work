74a3e5e6b36cb5288576021e0936038d9df9fb74
liupt <liupt@ziroom.com>
2020-04-21 17:58:37 +0800

    t

---
+++ 2020-04-21/liupt/5-7.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <string.h>
+#define MAXLINES 5000 /* max #lines to be sorted */
+#define MAXLEN 1000
+
+/* max length of any input line */
+int agetline(char *, int);
+char *lineptr[MAXLINES]; /* pointers to text lines */
+
+int readlines(char *lineptr[], char *linestor, int nlines);
+void writelines(char *lineptr[], int nlines);
+void qsort(char *lineptr[], int left, int right);
+/* sort input lines */
+int main()
+{
+    int nlines; /* number of input lines read */
+    char *linestor;
+    if ((nlines = readlines(lineptr, linestor, MAXLINES)) >= 0)
+    {
+        qsort(lineptr, 0, nlines - 1);
+        writelines(lineptr, nlines);
+        return 0;
+    }
+    else
+    {
+        printf("error: input too big to sort\n");
+        return 1;
+    }
+}
+
+/* readlines: read input lines */
+int readlines(char *lineptr[], char *linestor, int maxlines)
+{
+    int len, nlines;
+    char line[MAXLEN];
+    char *p = linestor;
+    char *lineptrstop = linestor + MAXLINES;
+    nlines = 0;
+    while ((len = agetline(line, MAXLEN)) > 0)
+        if (nlines >= maxlines || p + len > lineptrstop)
+            return -1;
+        else
+        {
+            line[len - 1] = '\0'; /* delete newline */
+            strcpy(p, line);
+            lineptr[nlines++] = p;
+        }
+    return nlines;
+}
+
+/* writelines: write output lines */
+void writelines(char *lineptr[], int nlines)
+{
+    while (nlines-- > 0)
+        printf("%s\n", *lineptr++);
+}
+
+/* qsort: sort v[left]...v[right] into increasing order */
+void qsort(char *v[], int left, int right)
+{
+    int i, last;
+    void swap(char *v[], int i, int j);
+    if (left >= right) /* do nothing if array contains */
+        return;        /* fewer than two elements */
+    swap(v, left, (left + right) / 2);
+    last = left;
+    for (i = left + 1; i <= right; i++)
+        if (strcmp(v[i], v[left]) < 0)
+            swap(v, ++last, i);
+    swap(v, left, last);
+    qsort(v, left, last - 1);
+    qsort(v, last + 1, right);
+}
+
+int agetline(char *s, int lim)
+{
+    int c, i;
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+    if (c == '\n')
+    {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
