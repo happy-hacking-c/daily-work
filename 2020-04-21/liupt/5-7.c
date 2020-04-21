489d39ebcab48dba8a517e5e1a826e236ffbe047
liupt <liupt@ziroom.com>
2020-04-21 18:28:14 +0800

    t

--- 2020-04-21/liupt/5-7.c
+++ 2020-04-21/liupt/5-7.c
@@ -37,13 +37,14 @@ int readlines(char *lineptr[], char *linestor, int maxlines)
     char *lineptrstop = linestor + MAXLINES;
     nlines = 0;
     while ((len = agetline(line, MAXLEN)) > 0)
-        if (nlines >= maxlines || p + len > lineptrstop)
+        if (nlines >= maxlines || (p + len) > lineptrstop)
             return -1;
         else
         {
             line[len - 1] = '\0'; /* delete newline */
             strcpy(p, line);
             lineptr[nlines++] = p;
+            p += len;
         }
     return nlines;
 }
