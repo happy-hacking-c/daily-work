4195067bb54ef7b55a934fa1dce722ab5bd7dae0
zhengx1 <zhengx1@ziroom.com>
2020-04-21 17:02:12 +0800

    up

--- 2020-04-21/zhengx1/main.c
+++ 2020-04-21/zhengx1/main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
 #define  MAXLINES 5000
+#define  MAXLEN 100
 #define ALLOCSIZE 10000
 
 static char allocbuf[ALLOCSIZE];
@@ -9,13 +10,17 @@ static char *allocp = allocbuf;
 
 char *lineptr[MAXLINES];
 int readlines(char *lineptr[],int nlines);
+int readlinesb(char *lineptr[],int nlines);
 char *alloc(int n);
 void afree(char *p);
 int mygetline(char line[],int maxline);
+char stringbuff[ALLOCSIZE];
+
 
 int main() {
     int nlines;
-    nlines = readlines(lineptr,MAXLINES);
+//    nlines = readlines(lineptr,MAXLINES);
+    nlines = readlinesb(lineptr,MAXLINES);
     printf("输入的总行数：%d\n",nlines);
     for (int i = 0; i < nlines; ++i) {
         printf("%s\n",lineptr[i]);
@@ -27,9 +32,9 @@ int main() {
 int readlines(char *lineptr[],int maxlines)
 {
     int len,nlines;
-    char *p ,line[MAXLINES];
+    char *p ,line[MAXLEN];
     nlines = 0;
-    while ((len = mygetline(line,MAXLINES)) >0) {
+    while ((len = mygetline(line,MAXLEN)) >0) {
         printf("输入的内容：%s,输入的长度：%d\n",line,len);
 //        return 0;
         if (nlines >= maxlines || (p = alloc(len)) == NULL)
@@ -38,6 +43,28 @@ int readlines(char *lineptr[],int maxlines)
             line[len -1] = '\0';
             strcpy(p,line);
             lineptr[nlines++] = p;
+        }
+    }
+    return nlines;
+
+}
+
+
+int readlinesb(char *lineptr[],int maxlines)
+{
+    int len, nlines;
+    char line[MAXLEN];
+    char *p = stringbuff;
+    char *linestop = stringbuff + ALLOCSIZE;
+    nlines = 0;
+    while((len = mygetline(line, MAXLEN)) > 0){
+        if(nlines >= maxlines || p + len > linestop)
+            return -1;
+        else{
+            line[len - 1] = '\0'; // 删除换行符
+            strcpy(p, line);
+            lineptr[nlines++] = p;
+            p += len;
         }
     }
     return nlines;
