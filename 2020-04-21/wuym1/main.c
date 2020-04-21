a8544ad9d817e206c7416a5b5907e9f08a77db02
wuym1 <wuym1@ziroom.com>
2020-04-21 14:37:42 +0800

    init

---
+++ 2020-04-21/wuym1/main.c
@@ -0,0 +1,43 @@
+#include "stdio.h"
+
+#define MAXLINES 5000
+
+char *lineptr[MAXLINES];
+
+int readlines(char *lineptr[], int nlines);
+void writelines(char *lineptr[], int nlines);
+
+void qsort(char *lineptr, int left, int right);
+
+int main() {
+    int nlines;
+    if ((nlines = readlines(lineptr, MAXLINES)) > 0) {
+        qsort(lineptr, 0, nlines - 1);
+        writelines(lineptr, nlines);
+        return 0;
+    } else {
+        printf("error: input too big to sort\n");
+        return 1;
+    }
+}
+
+#define MAXLEN 1000
+int getline(char *, int);
+char *alloc(int);
+
+int readlines(char *lineptr[], int maxlines) {
+    int len, nlines;
+    char *p, line[MAXLEN];
+
+    nlines = 0;
+    while ((len = getline(line, MAXLEN)) > 0) {
+        if (nlines >= maxlines || (p = alloc(len)) == NULL) {
+            return -1;
+        } else {
+            line[len-1] = '\0';
+            strcpy(p, line);
+            lineptr[nlines++] = p;
+        }
+    }
+    return nlines;
+}
