e48c2dbd084642a2a2619e27e2945ade27de2f04
zhenwx <allowall@163.com>
2020-04-22 13:09:22 +0800

    .

--- 2020-04-22/Q5-8.c
+++ 2020-04-22/Q5-8.c
@@ -6,8 +6,10 @@ static char daytab[2][13] = {
 
 /* day_of_year: set day of year from month & day */
 int day_of_year(int year, int month, int day) {
+    if (month > 12 || month < 1) return 0;
+
     int i, leap;
-    leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    leap = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
     for (i = 1; i < month; i++) day += daytab[leap][i];
     return day;
 }
@@ -15,7 +17,7 @@ int day_of_year(int year, int month, int day) {
 /* month_day: set month, day from day of year */
 void month_day(int year, int yearday, int *pmonth, int *pday) {
     int i, leap;
-    leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    leap = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
     for (i = 1; yearday > daytab[leap][i]; i++) yearday -= daytab[leap][i];
     *pmonth = i;
     *pday = yearday;
