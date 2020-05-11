62b9c85eccd8242be50d0a55ebc61a3bbc88a17d
zhengx1 <zhengx1@ziroom.com>
2020-05-11 14:18:12 +0800

    up

--- 2020-05-09/zhengx1/insert_sort.c
+++ 2020-05-09/zhengx1/insert_sort.c
@@ -2,9 +2,10 @@
 
 int main(int argc, char const *argv[])
 {
-	int arr[13] = {1,9,2,5,22,1,23,344,556,11,28,5,41};
+	int arr[] = {1,9,2,5,22,1,23,344,556,11,28,5,41};
+	int length = 13;
 
-	for (int i = 1; i < 13; ++i)
+	for (int i = 1; i < length; ++i)
 	{
 		int insertValue = arr[i];
 		int j = i-1;
@@ -16,11 +17,35 @@ int main(int argc, char const *argv[])
 		arr[j+1] = insertValue;
 	}
 
-	for (int i = 0; i < 13; ++i)
+	for (int i = 0; i < length; ++i)
 	{
 		printf("%d\n", arr[i]);
 		/* code */
 	}
+
+	int arr2[] = {1,9,2,5,22,1,23,344,556,11,28,5,41};
+
+	for (int i = 0; i < length-1; ++i)
+	{
+		int minIndex = i;
+		for (int j = i+1; j < length; ++j)
+		{
+			minIndex = arr2[j] < arr2[minIndex] ? j : minIndex;
+
+		}
+		int tmp = arr2[minIndex];
+		arr2[minIndex] = arr2[i];
+		arr2[i] = tmp;
+
+	}
+
+
+	for (int i = 0; i < length; ++i)
+	{
+		printf("%d\n", arr2[i]);
+		/* code */
+	}
+
 	return 0;
 }
 
