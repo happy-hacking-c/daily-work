e976fa14bff249cc6a467e4f79d2869a12ba8db4
zhenwx <allowall@163.com>
2020-05-11 15:42:09 +0800

    .

--- 2020-05-11/zhenwx/main.c
+++ 2020-05-11/zhenwx/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
 int mysort(int s[], int n) {
-    
+    int i, j, tmp;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (s[j] > s[j + 1]) {
+                tmp = s[j];
+                s[j] = s[j + 1];
+                s[j + 1] = tmp;
+            }
+        }
+    }
+
     return 0;
 }
 
