83aa5349f4930c896c52040ca2cbbe6b7ab429b7
bt212 <295551092@qq.com>
2020-05-15 15:42:35 +0800

    ;>

---
+++ 2020-05-15/liangc3/7-1.c
@@ -0,0 +1,17 @@
+//
+// Created by cotton on 2020/5/15.
+//
+#include <stdio.h>
+int main(int argc, char * argv[]) {
+    char * s = argv[0];
+    while (*s != '\0') {
+        if (*s >= 'a' && *s <= 'z') {
+            *s -= 32;
+        } else if (*s >= 'A' && *s <= 'Z') {
+            *s += 32;
+        }
+        s++;
+    }
+    printf("%s\n", argv[0]);
+}
+
