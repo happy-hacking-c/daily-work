ca3fdfcd05dab38edf3a93d96d551f38e9bd489e
bt212 <295551092@qq.com>
2020-05-16 18:48:19 +0800

    ;>

---
+++ 2020-05-18/liangc3/7-2.c
@@ -0,0 +1,33 @@
+//
+// Created by cotton on 2020/5/16.
+//
+#include <stdio.h>
+#include <ctype.h>
+#define MAXLINE  5
+int incPos(int pos, int inc);
+int main() {
+    int c;
+    int pos = 0;
+    while( (c = getchar()) != EOF) {
+        if (iscntrl(c) || c == ' ') {
+            pos = incPos(pos, 3);
+            printf("\\%02x", c);
+            if (c == '\n') {
+                putchar('\n');
+                pos = 0;
+            }
+        } else {
+            pos = incPos(pos, 1);
+            putchar(c);
+        }
+    }
+    return 0;
+}
+
+int incPos(int pos, int inc) {
+    if ((pos += inc) > MAXLINE) {
+        pos = inc;
+        putchar('\n');
+    }
+    return pos;
+}
