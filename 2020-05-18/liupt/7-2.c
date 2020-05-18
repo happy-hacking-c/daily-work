99fc68d1ceaac076ead33013645763852cb07527
liupt <liupt@ziroom.com>
2020-05-18 17:13:25 +0800

    t

---
+++ 2020-05-18/liupt/7-2.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<ctype.h>
+
+#define MAXLINE 10 /* maximum number of chars in one line */
+#define OCTLEN  6   /* length of an octal value */
+
+int inc(int pos,int n);
+
+
+int main(void)
+{
+    int c,pos;
+    pos = 0;        /* position in the line */
+
+    while((c=getchar()) != EOF)
+        if(iscntrl(c) || c == ' ')
+        {
+            /* non-graphic or blank character */
+            pos = inc(pos,OCTLEN);
+            printf("\\%03o",c);
+            /* newline character */
+            if(c == '\n')
+                pos = 0;
+            putchar('\n');
+        }
+        else
+        {
+            /* graphic character */
+            pos = inc(pos,1);
+            putchar(c);
+        }
+    return 0;
+}
+
+/* inc : increment position counter for output */
+int inc(int pos,int n)
+{
+    if(pos + n < MAXLINE)
+        return pos + n;
+    else
+    {
+        putchar('\n');
+        return n;
+    }
+}
