ba1230a5708c7da4e72b3b039c1baf3a6fad7251
zkbhj <zhengk3@ziroom.com>
2020-05-20 16:12:42 +0800

    增加备注信息

--- 2020-05-20/zhengk3/heap-sort.c
+++ 2020-05-20/zhengk3/heap-sort.c
@@ -14,21 +14,26 @@ void downAdjust(char *p, int parentIndex, int lenth)
 {
     //保存父节点
     int temp = *(p+parentIndex);
+    //根据堆的性质，可以根据父节点索引计算左孩子的索引
+    //parent(i) = floor((i - 1)/2)
+    //left(i)   = 2i + 1
+    //right(i)  = 2i + 2
     int childIndex = 2 * parentIndex + 1;
+    //从左孩子开始，一直寻找，孩子索引始终寻找比当前这个孩子大的节点
     while(childIndex < lenth) {
         //如果有右孩子，且右孩子大于左孩子，则定位到右孩子
         if(childIndex + 1 < lenth && *(p + childIndex + 1) > *(p + childIndex)) {
             childIndex++;
         }
 
-        //如果父节点小于任何一个孩子的值，直接跳出
-        if(temp > *(p + childIndex))
+        //如果父节点大于任何一个孩子（即比两个孩子节点）的值，直接跳出（无需调整，它就是最大的点）
+        if(temp >= *(p + childIndex))
            break;
 
-        //单向赋值
+        //需要调整，把最大值赋值给这个“父”节点即可
         *(p + parentIndex) = *(p + childIndex);
         parentIndex = childIndex;
-        childIndex = 2 * childIndex + 1;
+        childIndex = 2 * parentIndex + 1;
     }
 
     *(p + parentIndex) = temp;
@@ -42,12 +47,12 @@ void heapSort(char *p)
     while(*t++ != '\0')
         len++;
 
-    //将数组构建成二叉堆
+    //将数组构建成二叉堆（大堆，因为要实现从小到大排序）
     for(int i =(len - 2)/2; i >=0; i--){
         downAdjust(p, i, len);
     }
 
-    //循环删除堆顶元素，移到集合尾部，调节堆产生新的对顶
+    //循环删除堆顶元素，移到集合尾部，调节堆产生新的堆顶
     for(int i = len - 1; i >0; i--) {
         //最后一个元素和第一元素进行交换
         int tmp = *(p+i);
