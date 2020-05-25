a3faa40a38bdf5cdee0be28f9f8651cee0d423f7
zhenwx <allowall@163.com>
2020-05-25 15:17:00 +0800

    .

--- 2020-05-25/zhenwx/main.c
+++ 2020-05-25/zhenwx/main.c
@@ -1,10 +1,4 @@
-/*
- * Functions like isupper can be implemented to save space or to save time.
- * Explore both possibilities.
- */
-
 #include <stdio.h>
-#include <stdlib.h>
 
 int myisupper(int);
 
@@ -13,7 +7,7 @@ int main(void) {
 
     while ((c = getchar()) != EOF) {
         if (c == '\n') continue;
-        
+
         if (c >= 'A' && c <= 'Z')
             printf("true\n");
         else
